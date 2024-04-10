60853f7c2da0891c01d0f0eccd8263b07f313f68
B3nj4-u <pedro.ulloavalenzuela@gmail.com>
2024-04-09 23:25:13 -0400

    Cambio de colores

--- GridNaut/src/components/NoticiasTarjeta.jsx
+++ GridNaut/src/components/NoticiasTarjeta.jsx
@@ -1,15 +1,18 @@
-// NoticiasTarjeta.jsx
 import React from 'react';
 import './NoticiasTarjeta.css';
 import { Link } from 'react-router-dom';
 
 function NoticiasTarjeta({ noticia }) {
+  // Formatea la fecha de publicación
+  const fechaPublicacion = new Date(noticia.fecha_publicacion);
+  const fechaFormateada = `${fechaPublicacion.getDate().toString().padStart(2, '0')}-${(fechaPublicacion.getMonth() + 1).toString().padStart(2, '0')}-${fechaPublicacion.getFullYear()}`;
+
   return (
     <div className="noticias-tarjeta">
       <h2>{noticia.titulo}</h2>
+      <p className='fecha'>Fecha de publicación: {fechaFormateada}</p>
       <p>{noticia.contenido.substring(0, 100)}...</p>
       <img src={noticia.url_imagen} alt="Imagen de la noticia" />
-      <p>Fecha de publicación: {noticia.fecha_publicacion}</p>
       <Link to={`/noticias/${noticia.id}`}>Ver más</Link>
     </div>
   );
---
+++ GridNaut/src/components/Separador.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Separador.css';
+
+function Separador() {
+  return <div className="separador" />;
+}
+
+export default Separador;
