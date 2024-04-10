dec53b778f2c3ccb8535ebfe10bbd0a87ac75085
B3nj4-u <pedro.ulloavalenzuela@gmail.com>
2024-04-10 10:29:11 -0400

    Colores agrupados
    Cambiar en variables.css

--- GridNaut/src/pages/NoticiaDetalle.jsx
+++ GridNaut/src/pages/NoticiaDetalle.jsx
@@ -1,4 +1,3 @@
-// NoticiaDetalle.jsx
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import "./NoticiaDetalle.css"
@@ -14,12 +13,16 @@ function NoticiaDetalle() {
       .catch(error => console.error('Error:', error));
   }, [id]);
 
+  // Formatea la fecha de publicación
+  const fechaPublicacion = new Date(noticia.fecha_publicacion);
+  const fechaFormateada = `${fechaPublicacion.getDate().toString().padStart(2, '0')}/${(fechaPublicacion.getMonth() + 1).toString().padStart(2, '0')}/${fechaPublicacion.getFullYear()} ${fechaPublicacion.getHours().toString().padStart(2, '0')}:${fechaPublicacion.getMinutes().toString().padStart(2, '0')}`;
+
   return (
     <div className="noticia-detalle">
       <h1>{noticia.titulo}</h1>
+      <p className="fecha-publicacion">Fecha de publicación: {fechaFormateada}</p>
       <p>{noticia.contenido}</p>
       <img src={noticia.url_imagen} alt="Imagen de la noticia" />
-      <p>Fecha de publicación: {noticia.fecha_publicacion}</p>
     </div>
   );
 }
